e54df5c8de9e4d712ae2b8c42adeec3a5d5562d2
zayed utsho <zayedutsho@example.com>
2023-12-30 20:12:27 +0600

    signout added

--- src/comp/LogIn.jsx
+++ src/comp/LogIn.jsx
@@ -1,18 +1,36 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../firebase/firebase.init";
+import { useState } from "react";
 
 const LogIn = () => {
-  //   const auth = getAuth(app);
-  //   const provider = GoogleAuthProvider();
-
   const auth = getAuth(app);
+
+  const [user, setUser] = useState([]);
   const provider = new GoogleAuthProvider();
 
   const handleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user;
+        const loggedUser = result.user;
+
+        setUser(loggedUser);
         console.log(user);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        setUser(null);
+        console.log(result);
       })
       .catch((error) => {
         console.log("error", error.message);
@@ -20,7 +38,17 @@ const LogIn = () => {
   };
   return (
     <div>
-      <button onClick={handleSignIn}>google</button>
+      {user ? (
+        <button onClick={handleSignOut}>SignOut</button>
+      ) : (
+        <button onClick={handleSignIn}>google</button>
+      )}
+
+      {user && (
+        <div>
+          <h1>{user.displayName}</h1>
+        </div>
+      )}
     </div>
   );
 };
